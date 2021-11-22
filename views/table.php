0dbe8d3adf7133b92c0386ea319c471939a9e881
sajede2003 <sajededarzinia@gmail.com>
2021-11-22 13:38:24 +0330

    add table page

---
+++ views/table.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="lib/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="lib/bootstrap-rtl/bootstrap-rtl.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <title>table</title>
+</head>
+<main>
+  <div class="container">
+    <div class="content">
+
+
+      <table class="table table-striped border">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">تیتر</th>
+            <th scope="col">مثال2</th>
+            <th scope="col">توضیحات</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>محسن</td>
+            <td>جوادی</td>
+            <td>مازندران</td>
+            <td class="w-25">
+              <img src="img/Wallpaper.jpg" style="height: 160px;" class="img-fluid img-thumbnail" alt="">
+            </td>
+            <td class="w-25 ">
+              <p style="height: 100px; overflow: hidden;">
+
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان
+                گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و
+                برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی
+                می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و
+                متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی
+                الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید
+                داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان
+                مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود
+                طراحی اساسا مورد استفاده قرار گیرد.
+              </p>
+            </td>
+            <td>
+              <button class="btn btn-danger "> حذف</button>
+              <button class="btn btn-success"> ویرایش</button>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">1</th>
+            <td>محسن</td>
+            <td>جوادی</td>
+            <td>مازندران</td>
+            <td class="w-25">
+              <img src="img/Wallpaper.jpg" style="height: 160px;" class="img-fluid img-thumbnail" alt="">
+            </td>
+            <td class="w-25 ">
+              <p style="height: 100px; overflow: hidden;">
+
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان
+                گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و
+                برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی
+                می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و
+                متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی
+                الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید
+                داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان
+                مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود
+                طراحی اساسا مورد استفاده قرار گیرد.
+              </p>
+            </td>
+            <td >
+              <button class="btn btn-danger "> حذف</button>
+              <button class="btn btn-success"> ویرایش</button>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">1</th>
+            <td>محسن</td>
+            <td>جوادی</td>
+            <td>مازندران</td>
+            <td class="w-25">
+              <img src="img/Wallpaper.jpg" style="height: 160px;" class="img-fluid img-thumbnail" alt="">
+            </td>
+            <td class="w-25 ">
+              <p style="height: 100px; overflow: hidden;">
+
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان
+                گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و
+                برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی
+                می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و
+                متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی
+                الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید
+                داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان
+                مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود
+                طراحی اساسا مورد استفاده قرار گیرد.
+              </p>
+            </td>
+            <td>
+              <button class="btn btn-danger "> حذف</button>
+              <button class="btn btn-success"> ویرایش</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</main>
+
+<body>
+  <script src="lib/jquery/dist/jquery.min.js"></script>
+  <script src="lib/bootstrap/dist/js/bootstrap.min.js"></script>
+  <script src="js/app.js"></script>
+</body>
+
+</html>
